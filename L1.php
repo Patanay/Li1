c3b4d821ef62442d87e730ef0f7ed3bd1823b919
Patanay <79907401+Patanay@users.noreply.github.com>
2021-03-10 17:24:07 +0530

    Create L1.php

---
+++ L1.php
@@ -0,0 +1,28 @@
+<p id="demo"></p>
+<p id="demo2"></p>
+<script>
+v=loadXMLDoc();
+function loadXMLDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML =
+      this.responseText;
+    }
+  };
+xhttp.open("GET", "https://pagalbanaya.000webhostapp.com/Li1.php", true);
+  xhttp.send();
+}
+v1=loadXMLDoc1();
+function loadXMLDoc1() {
+  var xhttp1 = new XMLHttpRequest();
+  xhttp1.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo2").innerHTML =
+      this.responseText;
+    }
+  };
+xhttp1.open("GET", "https://pagalbanaya.000webhostapp.com/Li1.php", true);
+  xhttp1.send();
+}
+</script>
