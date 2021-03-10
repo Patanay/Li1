608fda8ba326eee7d3c47017582d5cb2f165b5b6
Patanay <79907401+Patanay@users.noreply.github.com>
2021-03-10 18:30:49 +0530

    Create Conn.php

---
+++ Conn.php
@@ -0,0 +1,11 @@
+<?php
+$conn=pg_connect("host=ec2-54-87-34-201.compute-1.amazonaws.com port=5432 dbname=dedlg7nokqgirj user=gkployqpqsndvv password=e6d2be1e0ad461a6a5cb012ce399dba704be2eb7f3f9ec0a55bbb93d5035784c");
+if($conn)
+{
+	echo"Successfull";
+	}
+	else{
+		echo "Connection Failed";
+		}
+		error_reporting(0);
+		?>
