85753b68a14c4b075316fdcba18b01cd819f8517
Patanay <79907401+Patanay@users.noreply.github.com>
2021-03-10 20:40:21 +0530

    Update Conn.php

--- Conn.php
+++ Conn.php
@@ -3,11 +3,12 @@
    $port        = "port = 5432";
    $dbname      = "dbname = testdb";
    $credentials = "user = postgres password=pass123";
-$db = pg_connect("host=ec2-54-87-34-201.compute-1.amazonaws.com port=5432 dbname=dedlg7nokqgirj user=gkployqpqsndv password=e6d2be1e0ad461a6a5cb012ce399dba704be2eb7f3f9ec0a55bbb93d5035784c");
+$db = pg_connect("host=ec2-54-87-34-201.compute-1.amazonaws.com port=5432 dbname=dedlg7nokqgirj user=gkployqpqsndvv password=e6d2be1e0ad461a6a5cb012ce399dba704be2eb7f3f9ec0a55bbb93d5035784c");
    //$db = pg_connect( "$host $port $dbname $credentials"  );
    if(!$db) {
       echo "Error : Unable to open database\n";
    } else {
       echo "Opened database successfully\n";
    }
+echo"Helo";
 ?>
