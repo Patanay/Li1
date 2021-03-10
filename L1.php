9a244d3fa1daee74399ec4606801d3dae874d309
Patanay <79907401+Patanay@users.noreply.github.com>
2021-03-10 17:28:12 +0530

    Update L1.php

--- L1.php
+++ L1.php
@@ -1,3 +1,5 @@
+<!DOCTYPE html>
+<html><body>
 <p id="demo"></p>
 <p id="demo2"></p>
 <script>
@@ -13,16 +15,4 @@ function loadXMLDoc() {
 xhttp.open("GET", "https://pagalbanaya.000webhostapp.com/Li1.php", true);
   xhttp.send();
 }
-v1=loadXMLDoc1();
-function loadXMLDoc1() {
-  var xhttp1 = new XMLHttpRequest();
-  xhttp1.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-      document.getElementById("demo2").innerHTML =
-      this.responseText;
-    }
-  };
-xhttp1.open("GET", "https://pagalbanaya.000webhostapp.com/Li1.php", true);
-  xhttp1.send();
-}
-</script>
+  </script></body></html>
