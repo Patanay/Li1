732fdd5a21e89b140218d21a207f87fb42da8472
Patanay <79907401+Patanay@users.noreply.github.com>
2021-03-12 19:07:40 +0530

    Update L1.php

--- L1.php
+++ L1.php
@@ -1,11 +1,4 @@
-<?php include("Conn.php");
-$query="SELECT *FROM movie1 ORDER BY id ASC ";
-$data=pg_query($conn,$query);
-while($result=pg_fetch_assoc($data))
-{ $Movie=$result['name'];
-echo "<a target=\"_top\" href=\"http://pagalbanaya.byethost3.com/Reindex.php?Movie=$Movie&L=https://moviesverse.org/?s=$Movie\"><img src=\"https://liwebsite.heliohost.org/3/Moviesflix/$Movie.jpg\" width=\"140\" height=\"260\" alt=\"Download $Movie\"></img></a>&nbsp;";
-}
-?>
+
 <?php include("Conn.php");
 $query="SELECT *FROM movie2 ORDER BY id ASC ";
 $data=pg_query($conn,$query);
