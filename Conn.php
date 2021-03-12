976980f5d398b3b958d48734ea9d5f5ec222d78f
Patanay <79907401+Patanay@users.noreply.github.com>
2021-03-12 16:50:44 +0530

    Update Conn.php

--- Conn.php
+++ Conn.php
@@ -1,8 +1,9 @@
 <?php
-   $db = pg_connect("host=ec2-54-87-34-201.compute-1.amazonaws.com port=5432 dbname=dedlg7nokqgirj user=gkployqpqsndvv password=e6d2be1e0ad461a6a5cb012ce399dba704be2eb7f3f9ec0a55bbb93d5035784c");
-   if(!$db) {
+   $conn = pg_connect("host=ec2-54-87-34-201.compute-1.amazonaws.com port=5432 dbname=dedlg7nokqgirj user=gkployqpqsndvv password=e6d2be1e0ad461a6a5cb012ce399dba704be2eb7f3f9ec0a55bbb93d5035784c");
+   if(!$conn) {
       echo "Error : Unable to open database\n"; 
    } else {
       echo "Opened database successfully\n";
    }
+error_reporting(0);
 ?>
