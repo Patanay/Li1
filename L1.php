9476249db218796653520e178ea9f5f6d6501ef2
Patanay <79907401+Patanay@users.noreply.github.com>
2021-03-12 19:12:05 +0530

    Update L1.php

--- L1.php
+++ L1.php
@@ -1,11 +1,8 @@
-
-<?php 
-echo "Hello";
-include("Conn.php");
-$query="SELECT *FROM movie2 ORDER BY id ASC ";
+<?php include("Conn.php");
+$query="SELECT *FROM movie1 ORDER BY id ASC ";
 $data=pg_query($conn,$query);
 while($result=pg_fetch_assoc($data))
 { $Movie=$result['name'];
-echo "<a target=\"_top\" href=\"http://pagalbanaya.byethost3.com/Reindex.php?Movie=$Movie&L=https://moviesflix.one/?s=$Movie\"><img src=\"https://liwebsite123.heliohost.us/Moviesflix/$Movie.jpg\" width=\"140\" height=\"260\" alt=\"Download $Movie\"></img></a>&nbsp;";
+echo "<a target=\"_top\" href=\"http://pagalbanaya.byethost3.com/Reindex.php?Movie=$Movie&L=https://moviesverse.org/?s=$Movie\"><img src=\"https://liwebsite.heliohost.org/3/Moviesflix/$Movie.jpg\" width=\"140\" height=\"260\" alt=\"Download $Movie\"></img></a>&nbsp;";
 }
 ?>
