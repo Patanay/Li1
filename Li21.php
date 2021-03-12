e1a66a4b4c37fd0376a0be53d292d89fd101a11a
Patanay <79907401+Patanay@users.noreply.github.com>
2021-03-12 17:22:52 +0530

    Update Li21.php

--- Li21.php
+++ Li21.php
@@ -1,1 +1,10 @@
-
+<?php error_reporting(0);
+$Name = $_POST['Name'];if($Name==""){}else{
+$str=rtrim($Name,",");
+include("Conn.php");
+$sql1="TRUNCATE TABLE movie1";
+pg_query($conn,$sql1);
+$sql="INSERT INTO movie1(Name) VALUES ".$str."";pg_query($conn,$sql);
+//header('Location: https://liwebsite123.heliohost.us/Li32.php');
+}
+?>
