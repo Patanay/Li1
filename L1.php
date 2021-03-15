6a4ab34fe1d183211889088dd546e69023319b16
Patanay <79907401+Patanay@users.noreply.github.com>
2021-03-15 19:36:31 +0530

    Update L1.php

--- L1.php
+++ L1.php
@@ -11,6 +11,22 @@ $query="SELECT *FROM movie2 ORDER BY id ASC ";
 $data=pg_query($conn,$query);
 while($result=pg_fetch_assoc($data))
 { $Movie=$result['name'];
-echo "<a target=\"_top\" href=\"http://pagalbanaya.byethost3.com/Reindex.php?Movie=$Movie&L=https://moviesflix.one/?s=$Movie\"><img src=\"https://liwebsite123.heliohost.us/Moviesflix/$Movie.jpg\" width=\"140\" height=\"260\" alt=\"Download $Movie\"></img></a>&nbsp;";
+echo "<a target=\"_top\" href=\"http://pagalbanaya.byethost3.com/Reindex.php?Movie=$Movie&L=https://altmovies.in/?s=$Movie\"><img src=\"https://liwebsite123.heliohost.us/Moviesflix/$Movie.jpg\" width=\"140\" height=\"260\" alt=\"Download $Movie\"></img></a>&nbsp;";
+}
+?>
+<?php include("Conn.php");
+$query="SELECT *FROM movie3 ORDER BY id ASC ";
+$data=pg_query($conn,$query);
+while($result=pg_fetch_assoc($data))
+{ $Movie=$result['name'];
+echo "<a target=\"_top\" href=\"http://pagalbanaya.byethost3.com/Reindex.php?Movie=$Movie&L=https://moviesverse.org/?s=$Movie\"><img src=\"https://pagalbana.000webhostapp.com/Moviesflix/$Movie.jpg\" width=\"140\" height=\"260\" alt=\"Download $Movie\"></img></a>&nbsp;";
+}
+?>
+<?php include("Conn.php");
+$query="SELECT *FROM movie4 ORDER BY id ASC ";
+$data=pg_query($conn,$query);
+while($result=pg_fetch_assoc($data))
+{ $Movie=$result['name'];
+echo "<a target=\"_top\" href=\"http://pagalbanaya.byethost3.com/Reindex.php?Movie=$Movie&L=https://altmovies.in/?s=$Movie\"><img src=\"https://pagalbanaya.000webhostapp.com/Moviesflix/$Movie.jpg\" width=\"140\" height=\"260\" alt=\"Download $Movie\"></img></a>&nbsp;";
 }
 ?>
