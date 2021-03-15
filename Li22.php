78ea20b09b85b7f4c6305e8432702f1ee7235e45
Patanay <79907401+Patanay@users.noreply.github.com>
2021-03-15 19:29:49 +0530

    Update Li22.php

--- Li22.php
+++ Li22.php
@@ -5,6 +5,6 @@ include("Conn.php");
 $sql1="TRUNCATE TABLE movie2";
 pg_query($conn,$sql1);
 $sql="INSERT INTO movie2(Name) VALUES ".$str."";pg_query($conn,$sql);}
-//header('Location: https://liwebsite123.heliohost.us/Li32.php');
+header('Location: https://pagalbana.000webhostapp.com/Li33.php');
 
 ?>
