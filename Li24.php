2abd8cd967c46459551f69795bd7df92e298e76f
Patanay <79907401+Patanay@users.noreply.github.com>
2021-03-15 19:32:19 +0530

    Create Li24.php

---
+++ Li24.php
@@ -0,0 +1,10 @@
+<?php error_reporting(0);
+$Name = $_POST['Name'];if($Name==""){}else{
+$str=rtrim($Name,",");
+include("Conn.php");
+$sql1="TRUNCATE TABLE movie4";
+pg_query($conn,$sql1);
+$sql="INSERT INTO movie4(Name) VALUES ".$str."";pg_query($conn,$sql);}
+header('Location: https://liwebsite1.herokuapp.com/Li1.php');
+
+?>
