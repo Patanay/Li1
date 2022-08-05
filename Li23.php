994395a207d03d30e360f1429ba2e74fdc36616f
Patanay <79907401+Patanay@users.noreply.github.com>
2022-08-05 08:00:06 +0530

    Update Li23.php

--- Li23.php
+++ Li23.php
@@ -5,6 +5,6 @@ include("Conn.php");
 $sql1="TRUNCATE TABLE movie3";
 pg_query($conn,$sql1);
 $sql="INSERT INTO movie3(Name) VALUES ".$str."";pg_query($conn,$sql);}
-header('Location: https://pagalbanaya.000webhostapp.com/Li34.php');
+header('Location: https://pagalbann.000webhostapp.com/A2/Li34.php');
 
 ?>
